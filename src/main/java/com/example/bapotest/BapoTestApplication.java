185dec1a2dc234b6f52c05d186d7297db0730db8
hanaxauay <hana0_0aa@naver.com>
2024-02-24 06:20:21 +0900

    first

---
+++ src/main/java/com/example/bapotest/BapoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bapotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bapotest/BapoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bapotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
